7f513b2f1e5d1cf6ef1387a377191de3f56a3f89
Min0Gyu0Kang <myname@g.skku.edu>
2025-07-02 14:35:56 +0900

    svg graph example code

--- vue-example/src/App.vue
+++ vue-example/src/App.vue
@@ -1,47 +1,86 @@
+<!--
+An SVG graph
+-->
+
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import PolyGraph from './PolyGraph.vue'
+import { ref, reactive } from 'vue'
+
+const newLabel = ref('')
+const stats = reactive([
+  { label: 'A', value: 100 },
+  { label: 'B', value: 100 },
+  { label: 'C', value: 100 },
+  { label: 'D', value: 100 },
+  { label: 'E', value: 100 },
+  { label: 'F', value: 100 }
+])
+
+function add(e) {
+  e.preventDefault()
+  if (!newLabel.value) return
+  stats.push({
+    label: newLabel.value,
+    value: 100
+  })
+  newLabel.value = ''
+}
+
+function remove(stat) {
+  if (stats.length > 3) {
+    stats.splice(stats.indexOf(stat), 1)
+  } else {
+    alert("Can't delete more!")
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <svg width="200" height="200">
+    <PolyGraph :stats="stats"></PolyGraph>
+  </svg>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <!-- controls -->
+  <div v-for="stat in stats">
+    <label>{{stat.label}}</label>
+    <input type="range" v-model="stat.value" min="0" max="100">
+    <span>{{stat.value}}</span>
+    <button @click="remove(stat)" class="remove">X</button>
+  </div>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <form id="add">
+    <input name="newlabel" v-model="newLabel">
+    <button @click="add">Add a Stat</button>
+  </form>
+
+  <pre id="raw">{{ stats }}</pre>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+polygon {
+  fill: #42b983;
+  opacity: 0.75;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+circle {
+  fill: transparent;
+  stroke: #999;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+text {
+  font-size: 10px;
+  fill: #666;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+label {
+  display: inline-block;
+  margin-left: 10px;
+  width: 20px;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#raw {
+  position: absolute;
+  top: 0;
+  left: 300px;
 }
 </style>
